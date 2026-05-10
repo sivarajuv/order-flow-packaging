aa792a816cf9b9e42ff9a17d217aa8696396a1c9
sivaraju <siva82k@gmail.com>
2026-05-10 11:39:49 +0530

    Delete orderflow/src directory

--- orderflow/src/branding.js
+++
@@ -1,9 +0,0 @@
-import logo from './assets/abhishek-polyplast-logo.png'
-
-export const BRAND = {
-  shortName: 'AP',
-  name: 'Abhishek PolyPlast',
-  subtitle: 'Sales & production',
-  assistantName: 'Abhishek PolyPlast Assistant',
-  logo,
-}
